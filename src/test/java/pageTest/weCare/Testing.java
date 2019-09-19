7e4d559e3bc802b1314dc0823c03fd1f01a1d337
khaledzaman <khaleduz.zaman@experis.com>
2019-09-19 15:05:06 -0400

    Updated Test

--- src/test/java/pageTest/weCare/Testing.java
+++ src/test/java/pageTest/weCare/Testing.java
@@ -1,6 +1,8 @@
 package pageTest.weCare;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.testng.Assert;
@@ -8,9 +10,11 @@ import org.testng.annotations.Test;
 
 public class Testing {
 
-	
 	@Test(priority = 0)
     public void test() {
-		Assert.assertFalse(false, "Hello");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		String date = sdf.format(new Date());
+		System.out.println(date);
 	}
+	
 }
